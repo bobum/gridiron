0933ea8c36ff03c1948ab079d636bc7475dbdc16
scott davis <scott.davis@semaphoredoes.com>
2016-02-23 01:44:27 -0500

    first commit

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+  db: 'mongodb://bobum:jeg4ewam@ds011258.mongolab.com:11258/bobum',
   debug: true,
   logging: {
     format: 'tiny'
