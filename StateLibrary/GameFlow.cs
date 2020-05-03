71b95f319232b451583bca962979229b02673e7e
bobum <scott@davisplanet.com>
2020-05-03 00:00:02 -0400

    adding new library in .net core with stateless vs. workflow - new unit test project as well

---
+++ StateLibrary/GameFlow.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DomainObjects;
+using Stateless;
+using Stateless.Graph;
+
+namespace StateLibrary
+{
+    public class GameFlow
+    {
+        enum Trigger
+        {
+            Snap,
+            TeamsSelected,
+            CoinTossed
+        }
+
+        enum State
+        {
+            PreGame,
+            CoinToss,
+            PrePlay,
+            FieldGoal,
+            RunPlay,
+            Kickoff,
+            Punt,
+            PassPlay,
+            FieldGoalBlock,
+            PuntBlock,
+            InterceptionReturn,
+            FumbleReturn,
+            FieldGoalResult,
+            RunPlayResult,
+            KickoffResult,
+            PuntResult,
+            PassResult,
+            PostPlay,
+            PostGame
+        }
+
+        private State _state = State.PreGame;
+
+        StateMachine<State, Trigger> _machine;
+        StateMachine<State, Trigger>.TriggerWithParameters<Game> _coinTossedTrigger;
+        StateMachine<State, Trigger>.TriggerWithParameters<Game> _setSnapTrigger;
+        StateMachine<State, Trigger>.TriggerWithParameters<Game> _teamsChosenTrigger;
+
+        public GameFlow(Game game)
+        {
+            _machine = new StateMachine<State, Trigger>(() => _state, s => _state = s);
+
+            _setSnapTrigger = _machine.SetTriggerParameters<Game>(Trigger.Snap);
+            _coinTossedTrigger = _machine.SetTriggerParameters<Game>(Trigger.CoinTossed);
+            _teamsChosenTrigger = _machine.SetTriggerParameters<Game>(Trigger.TeamsSelected);
+
+            _machine.Configure(State.PreGame)
+                .Permit(Trigger.TeamsSelected, State.CoinToss);
+
+            _machine.Configure(State.CoinToss)
+                .OnEntryFrom(_teamsChosenTrigger, OnTeamsChosen, "Teams Chosen")
+                .Permit(Trigger.CoinTossed, State.PrePlay);
+
+            _machine.OnTransitioned(t =>
+                Console.WriteLine(
+                    $"OnTransitioned: {t.Source} -> {t.Destination} via {t.Trigger}({string.Join(", ", t.Parameters)})"));
+
+            _machine.Fire(_teamsChosenTrigger, game);
+        }
+
+        private void OnTeamsChosen(Game game)
+        {
+            var test = game;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+using DomainObjects;
+using DomainObjects.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StateLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var phoneCall = new PhoneCall("Lokesh"); //<-- this would be adding the home and away teams - initializing the game object
+            phoneCall.Print();
+            phoneCall.Dialed("Prameela");
+            phoneCall.Print();
+            phoneCall.Connected();
+            phoneCall.Print();
+            phoneCall.SetVolume(2);
+            phoneCall.Print();
+            phoneCall.Hold();
+            phoneCall.Print();
+            phoneCall.Mute();
+            phoneCall.Print();
+            phoneCall.Unmute();
+            phoneCall.Print();
+            phoneCall.Resume();
+            phoneCall.Print();
+            phoneCall.SetVolume(11);
+            phoneCall.Print();
+        }
+
+        [TestMethod]
+        public void GameTest()
+        {
+            var game = GameHelper.GetNewGame();
+            var gameFlow = new GameFlow(game);
+        }
+    }
+}
